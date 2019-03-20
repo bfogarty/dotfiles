4d7120c6e2cc9995bb6dd28f3e79fc25f6b344cc
Brian Fogarty <bri@nfogarty.me>
2019-03-20 11:37:12 -0400

    Add fish aliases for MacVim, httpie, and Git WIP

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -6,6 +6,16 @@ alias l 'ls -al'
 # verbose stat
 alias stat 'stat -x'
 
+# mvim
+alias mvim 'pipenv run /Applications/MacVim.app/Contents/bin/mvim'
+
+# use vim colors for httpie
+alias http 'http --style=vim'
+
+# useful git WIP commands
+alias wip 'git commit -am "WIP"'
+alias unwip 'git reset --soft HEAD~1; git reset .'
+
 # keep count of how many cups of coffee I've had at work
 alias cups 'echo (cat ~/.coffee) cups of coffee'
 alias drink 'echo (math (cat ~/.coffee) + 1) > ~/.coffee; and cups'
