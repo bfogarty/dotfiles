4249c90b6d72dede1387e4ab47bc524ef8afdd2d
Brian Fogarty <bri@nfogarty.me>
2019-03-22 10:18:54 -0400

    Stage new files when using wip alias

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -13,7 +13,7 @@ alias mvim 'pipenv run /Applications/MacVim.app/Contents/bin/mvim'
 alias http 'http --style=vim'
 
 # useful git WIP commands
-alias wip 'git commit -am "WIP"'
+alias wip 'git add .;git commit -m "WIP"'
 alias unwip 'git reset --soft HEAD~1; git reset .'
 
 # keep count of how many cups of coffee I've had at work
